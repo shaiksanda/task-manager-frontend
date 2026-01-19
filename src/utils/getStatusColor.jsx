76995e77677bbebd9263f7294b6bde8f6a5c14be
shaiksanda <shaiksandanibasha7@gmail.com>
2026-01-19 11:59:38 +0530

    fix: change color for pending tasks

--- src/utils/getStatusColor.jsx
+++ src/utils/getStatusColor.jsx
@@ -3,8 +3,6 @@ export const getStatusColor = (status) => {
     case "completed":
       return "green";
     case "pending":
-      return "orange";
-    case "missed":
       return "red";
     default:
       return "gray";
